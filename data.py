bb4271545c5af1b16a9162a5490a5d49601df509
Dan <guaneec@gmail.com>
2019-12-13 22:01:04 +0800

    Add script to fetch input

---
+++ data.py
@@ -0,0 +1,26 @@
+import requests
+from os import makedirs
+from pathlib import Path
+
+with open("session.txt") as f:
+    cookies = {"session": f.read().strip()}
+
+datadir = Path('./data')
+
+
+makedirs(datadir, exist_ok=True)
+
+for i in range(1,26):
+    filepath = datadir / f'{i:02d}.input.txt'
+    if filepath.is_file():
+        continue
+
+    url = f'https://adventofcode.com/2019/day/{i}/input'
+    r = requests.get(url, cookies=cookies)
+    if r.status_code != 200:
+        print(f'failed fetching input for day {i} (status={r.status_code})')
+        break
+    
+    with open(filepath, 'w') as f:
+        f.write(r.text)
+        print(f'fetch {str(filepath)}')
