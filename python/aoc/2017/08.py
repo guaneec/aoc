547a56c3a53df38f204dc6df55ffc4c8ca187b39
Dan <guaneec@gmail.com>
2019-12-15 22:44:29 +0800

    Python 2017.08

--- python/aoc/2017/08.py
+++ python/aoc/2017/08.py
@@ -1,3 +1,25 @@
+from collections import defaultdict
+
 from .util import getinput
 
-print(getinput(8))
+s = getinput(8)
+
+vars = defaultdict(int)
+
+ls = s.splitlines()
+mm = 0
+
+for l in ls:
+    x, op, d, _, y, cmp, i = l.split(' ')
+    if  (cmp == '>' and vars[y] > int(i)
+    or cmp == '<' and vars[y] < int(i)
+    or cmp == '==' and vars[y] == int(i)
+    or cmp == '>=' and vars[y] >= int(i)
+    or cmp == '<=' and vars[y] <= int(i)
+    or cmp == '!=' and vars[y] != int(i)):
+        if op == 'inc': vars[x] += int(d)
+        if op == 'dec': vars[x] -= int(d)
+        mm = max(vars[x], mm)
+
+print(max(v for v in vars.values()))
+print(mm)
