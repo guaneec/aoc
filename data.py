09132e78f4c350c972f7801cf83d213de7254904
Dan <guaneec@gmail.com>
2019-12-15 20:38:41 +0800

    Fetch data for 2017 2018

--- data.py
+++ data.py
@@ -8,21 +8,23 @@ with open(Path(__file__) / "../session.txt") as f:
 datadir = Path(__file__) / ('../data')
 
 
-makedirs(datadir, exist_ok=True)
 
 print("fetching inputs...")
 
-for i in range(1,26):
-    filepath = datadir / f'{i:02d}.input.txt'
-    if filepath.is_file():
-        continue
+for y in [2017, 2018, 2019]:
+    diryear = datadir / str(y)
+    makedirs(diryear, exist_ok=True)
+    for i in range(1,26):
+        filepath = diryear / f'{i:02d}.input.txt'
+        if filepath.is_file():
+            continue
 
-    url = f'https://adventofcode.com/2019/day/{i}/input'
-    r = requests.get(url, cookies=cookies)
-    if r.status_code != 200:
-        print(f'failed fetching input for day {i} (status={r.status_code})')
-        break
-    
-    with open(filepath, 'w') as f:
-        f.write(r.text)
-        print(f'fetched {str(filepath)}')
+        url = f'https://adventofcode.com/{y}/day/{i}/input'
+        r = requests.get(url, cookies=cookies)
+        if r.status_code != 200:
+            print(f'failed fetching input for day {i} (status={r.status_code})')
+            break
+        
+        with open(filepath, 'w') as f:
+            f.write(r.text)
+            print(f'fetched {str(filepath)}')
