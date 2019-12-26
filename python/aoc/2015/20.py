b1a0b1283d0190938330e397a8dc52acbf55f650
Dan <guaneec@gmail.com>
2019-12-26 22:57:31 +0800

    Python 2015.20 speedup

--- python/aoc/2015/20.py
+++ python/aoc/2015/20.py
@@ -1,18 +1,22 @@
 from .util import getinput
-from sympy.ntheory import abundance, divisor_sigma, divisors, factorint
+import numpy as np
 import doctest
 from itertools import count
 
 def p1(x):
-    for i in count(x // 60):
-        if 10 * divisor_sigma(i) >= x:
-            return i
+    x = (x + 9) // 10
+    a = np.zeros((x,))
+    for i in range(1, x):
+        a[i-1::i] += i
+    return np.nonzero(a >= x)[0][0]+1
+    
 
 def p2(x):
-    xx = (x + 10) // 11
-    for i in count(xx // 6):
-        if sum( a for a in divisors(i) if a >= i // 50 ) >= xx:
-            return i
+    x = (x + 10) // 11
+    a = np.zeros((x,))
+    for i in range(1, x):
+        a[i-1:i*50:i] += i
+    return np.nonzero(a >= x)[0][0]+1
 
 x = int(getinput(20))
 print(p1(x))
