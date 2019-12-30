39af7fa825dc7a0f40dac28ae92969197cd5f090
Dan <guaneec@gmail.com>
2019-12-30 16:26:53 +0800

    Python 2016.12 speedup

--- python/aoc/2016/12.py
+++ python/aoc/2016/12.py
@@ -3,6 +3,18 @@ import doctest
 from collections import defaultdict
 import re
 
+'''
+a,b,d = 1,1,26
+
+if c:
+    d+=7
+
+repeat d:
+    a, b = a+b, a 
+
+a += 16 * 12
+
+'''
 
 def run(code, init=None):
     i = 0
@@ -29,6 +41,16 @@ def run(code, init=None):
 
 s = getinput(12)
 
+a, b = 1, 1
+for i in range(26+7):
+    a, b = a+b, a
+    if i == 26 - 1:
+        print(a+ 16 * 12)
+    if i == 26 + 7 - 1:
+        print(a + 16 * 12)
+
+"""
 code = [(l[:3], l[4:].split()) for l in s.strip().splitlines() ]
 print(run(code)['a'])
 print(run(code, {'c': 1})['a'])
+"""
