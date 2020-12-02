7577089662022aac5935152ab4b110306e05ae05
Dan <guaneec@gmail.com>
2020-12-03 01:25:34 +0800

    Slight speedup

--- nim/2020/D01.nim
+++ nim/2020/D01.nim
@@ -1,21 +1,30 @@
 from sugar import collect
 import strutils
+import algorithm
 import ../aoc
 
-let nums = collect(newSeq):
+var nums = collect(newSeq):
     for line in splitLines(aocin(2020, 1).strip()): line.parseInt()
-let n = nums.len
 
-block b1:
-    for i in countup(0, n - 1):
-        for j in countup(i + 1, n - 1):
-            if nums[i] + nums[j] == 2020:
-                echo nums[i] * nums[j]
-                break b1
-block b2:
-    for i in countup(0, n - 1):
-        for j in countup(i + 1, n - 1):
-            for k in countup(j + 1, n - 1):
-                if nums[i] + nums[j] + nums[k] == 2020:
-                    echo nums[i] * nums[j] * nums[k]
-                    break b2
+nums.sort()
+
+proc findsum(l: seq[int], target: int): (int, int) =
+    var (i, j) = (0, l.len - 1)
+    result = (-1, -1)
+    while i < j:
+        let ss = l[i] + l[j]
+        if ss == target:
+            return (l[i], l[j])
+        if ss < target:
+            i += 1
+        else:
+            j -= 1
+    
+let (x1, x2) = findsum(nums, 2020)
+echo x1 * x2
+
+for i in 0 .. nums.len-1:
+    let (y1, y2) = findsum(nums[i .. ^1], 2020 - nums[i])
+    if y1 >= 0:
+        echo nums[i] * y1 * y2
+        break
