c5012ed9ee975a98af0f601dadc7f010c2f9a164
dan <guaneec@gmail.com>
2022-12-14 14:54:23 +0800

    Python 2022.14 recursive

--- python/aoc/2022/14.py
+++ python/aoc/2022/14.py
@@ -22,27 +22,27 @@ for l in s.splitlines():
             floor = max(floor, b + 2)
         pa, pb = a, b
 
-def drop(a, b):
-    while True:
-        try:
-            b = min(x for x in g[a] if x >= b) - 1
-        except:
-            return a, floor - 1
-        if b+1 not in g[a-1]:
-            a, b = a-1, b+1
-        elif b+1 not in g[a+1]:
-            a, b = a+1, b+1
-        else:
-            return a, b
+def drop2(a, b):
+    if b == floor or b in g[a]:
+        return 0
+    o = drop2(a, b+1) + drop2(a-1, b+1) + drop2(a+1, b+1)
+    g[a].add(b)
+    return o + 1
 
-for part1 in (True, False):
-    gt = deepcopy(g)
-    t = 0
-    while True:
-        t += 1
-        a, b = drop(500, 0)
-        if (b == floor - 1 if part1 else (a, b) == (500, 0)):
-            break
-        g[a].add(b)
-    print(t - part1)
-    g = gt
+def drop1(a, b):
+    if b == floor - 1:
+        return 0, True
+    if b in g[a]:
+        return 0, False
+    g[a].add(b)
+    o = 0
+    for x, leaks in (drop1(aa, b+1) for aa in (a, a-1, a+1)):
+        o += x
+        if leaks:
+            return o, True
+    return o + 1, False
+
+gt = deepcopy(g)
+print(drop1(500, 0)[0])
+g = gt
+print(drop2(500, 0))
