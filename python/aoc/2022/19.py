51b2032321b20206c9d623dfca490498a0f8152a
dan <guaneec@gmail.com>
2022-12-19 17:05:05 +0800

    Python 2022.19 optims (pypy 5s->1.4s)

--- python/aoc/2022/19.py
+++ python/aoc/2022/19.py
@@ -16,15 +16,14 @@ cdiv = lambda a, b: 1e8 if b == 0 else (a+b-1) // b
 def f(T, b, state):
     t, ore, clay, obs, geo, dore, dclay, dobs, dgeo = state
     oo, co, obo, obc, go, gob = bps[b]
-    tt = T + 1 - t
     m = geo + (T + 1 - t) * dgeo
-    if dore * tt + ore < max(oo, co, obo, go) * tt and (dt := cdiv(max(0, oo - ore), dore) + 1) + t <= T + 1:
+    if (tt := T-2-t) > 0 and dore * (tt-1) + ore < max(oo, co, obo, go) * tt and (dt := cdiv(max(0, oo - ore), dore) + 1) + t <= T:
         m = max(m, f(T, b, (t+dt, ore+dt*dore-oo, clay+dt*dclay, obs+dt*dobs, geo+dt*dgeo, dore+1, dclay, dobs, dgeo)))
-    if dclay * tt + clay < obc * tt and (dt := cdiv(max(0, co - ore), dore) + 1) + t <= T + 1:
+    if (tt := T-4-t) > 0 and dclay * (tt-1) + clay < obc * tt and (dt := cdiv(max(0, co - ore), dore) + 1) + t <= T:
         m = max(m, f(T, b, (t+dt, ore+dt*dore-co, clay+dt*dclay, obs+dt*dobs, geo+dt*dgeo, dore, dclay+1, dobs, dgeo)))
-    if dobs * tt + obs < gob * tt and (dt := max(cdiv(max(0, obo - ore), dore), cdiv(max(0, obc - clay), dclay)) + 1) + t <= T + 1:
+    if (tt := T-2-t) > 0 and dobs * (tt-1) + obs < gob * tt and (dt := max(cdiv(max(0, obo - ore), dore), cdiv(max(0, obc - clay), dclay)) + 1) + t <= T:
         m = max(m, f(T, b, (t+dt, ore+dt*dore-obo, clay+dt*dclay-obc, obs+dt*dobs, geo+dt*dgeo, dore, dclay, dobs+1, dgeo)))
-    if (dt := max(cdiv(max(0, go - ore), dore), cdiv(max(0, gob - obs), dobs)) + 1) + t <= T + 1:
+    if (dt := max(cdiv(max(0, go - ore), dore), cdiv(max(0, gob - obs), dobs)) + 1) + t <= T:
         m = max(m, f(T, b, (t+dt, ore+dt*dore-go, clay+dt*dclay, obs+dt*dobs-gob, geo+dt*dgeo, dore, dclay, dobs, dgeo+1)))
     return m
         
