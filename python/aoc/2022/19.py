d78ca7f47610520490e0f725fcab82b312592d60
dan <guaneec@gmail.com>
2022-12-19 15:36:49 +0800

    Python 2022.19 optims (pypy 16s->5s)

--- python/aoc/2022/19.py
+++ python/aoc/2022/19.py
@@ -15,21 +15,18 @@ cdiv = lambda a, b: 1e8 if b == 0 else (a+b-1) // b
 
 def f(T, b, state):
     t, ore, clay, obs, geo, dore, dclay, dobs, dgeo = state
-    if t > T:
-        assert t == T + 1
-        yield geo
-        return
-    oo, co, obo, obc, go, gob = b
+    oo, co, obo, obc, go, gob = bps[b]
     tt = T + 1 - t
+    m = geo + (T + 1 - t) * dgeo
     if dore * tt + ore < max(oo, co, obo, go) * tt and (dt := cdiv(max(0, oo - ore), dore) + 1) + t <= T + 1:
-        yield from f(T, b, (t+dt, ore+dt*dore-oo, clay+dt*dclay, obs+dt*dobs, geo+dt*dgeo, dore+1, dclay, dobs, dgeo))
+        m = max(m, f(T, b, (t+dt, ore+dt*dore-oo, clay+dt*dclay, obs+dt*dobs, geo+dt*dgeo, dore+1, dclay, dobs, dgeo)))
     if dclay * tt + clay < obc * tt and (dt := cdiv(max(0, co - ore), dore) + 1) + t <= T + 1:
-        yield from f(T, b, (t+dt, ore+dt*dore-co, clay+dt*dclay, obs+dt*dobs, geo+dt*dgeo, dore, dclay+1, dobs, dgeo))
+        m = max(m, f(T, b, (t+dt, ore+dt*dore-co, clay+dt*dclay, obs+dt*dobs, geo+dt*dgeo, dore, dclay+1, dobs, dgeo)))
     if dobs * tt + obs < gob * tt and (dt := max(cdiv(max(0, obo - ore), dore), cdiv(max(0, obc - clay), dclay)) + 1) + t <= T + 1:
-        yield from f(T, b, (t+dt, ore+dt*dore-obo, clay+dt*dclay-obc, obs+dt*dobs, geo+dt*dgeo, dore, dclay, dobs+1, dgeo))
+        m = max(m, f(T, b, (t+dt, ore+dt*dore-obo, clay+dt*dclay-obc, obs+dt*dobs, geo+dt*dgeo, dore, dclay, dobs+1, dgeo)))
     if (dt := max(cdiv(max(0, go - ore), dore), cdiv(max(0, gob - obs), dobs)) + 1) + t <= T + 1:
-        yield from f(T, b, (t+dt, ore+dt*dore-go, clay+dt*dclay, obs+dt*dobs-gob, geo+dt*dgeo, dore, dclay, dobs, dgeo+1))
-    yield geo + (T + 1 - t) * dgeo
+        m = max(m, f(T, b, (t+dt, ore+dt*dore-go, clay+dt*dclay, obs+dt*dobs-gob, geo+dt*dgeo, dore, dclay, dobs, dgeo+1)))
+    return m
         
-print(sum(k * max(f(24, v, (1, 0, 0, 0, 0, 1, 0, 0, 0))) for k, v in bps.items()))
-print(prod(max(f(32, v, (1, 0, 0, 0, 0, 1, 0, 0, 0))) for k, v in bps.items() if k <= 3))
+print(sum(k * (f(24, k, (1, 0, 0, 0, 0, 1, 0, 0, 0))) for k in bps))
+print(prod((f(32, k, (1, 0, 0, 0, 0, 1, 0, 0, 0))) for k in bps if k <= 3))
