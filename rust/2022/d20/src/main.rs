659ac7df7148873113ca6b6bbb06f922bcba34bf
dan <guaneec@gmail.com>
2022-12-20 18:43:14 +0800

    Rust 2022.20 opt (.4s->.1s)

--- rust/2022/d20/src/main.rs
+++ rust/2022/d20/src/main.rs
@@ -2,27 +2,19 @@ fn main() {
     let s = std::fs::read_to_string("../../../data/2022/20.input.txt").unwrap();
     let a: Vec<i32> = s.split_terminator('\n').map(|x| x.parse().unwrap()).collect();
     let n = a.len();
+    let nn = (n - 1) as i64;
     for (mult, rounds) in [(1, 1), (811589153, 10)] {
         let a: Vec<i64> = a.iter().map(|&x| x as i64 * mult).collect();
         let mut ixs: Vec<i64> = (0..n as i64).collect();
         for _ in 0..rounds {
             for (i, x) in a.iter().enumerate() {
-                let j = ixs[i];
-                let nn = n as i64 - 1;
-                let jj = ((j + x) % nn + nn) % nn;
-                if j < jj {
-                    for k in 0..n {
-                        ixs[k] -= if j < ixs[k] && ixs[k] <= jj {1} else if ixs[k] == j {j - jj} else {0};
-                    }
-                } else {
-                    for k in 0..n {
-                        ixs[k] += if jj <= ixs[k] && ixs[k] < j {1} else if ixs[k] == j {jj - j} else {0};
-                    }
-                }
+                let j = ixs.iter().position(|&ii| ii == i as i64).unwrap() as i64;
+                let jj = ((x + j) % nn + nn) % nn;
+                if j < jj { ixs[j as usize..=jj as usize].rotate_left(1); }
+                else { ixs[jj as usize..=j as usize].rotate_right(1); }
             }
         }
-        let i0 = ixs[a.iter().position(|&x| x == 0).unwrap()];
-        let targets: [i64; 3] = [1000, 2000, 3000].map(|di| (i0+di) % n as i64);
-        println!("{}", (0..n).filter(|&i| targets.contains(&ixs[i])).map(|i| a[i]).sum::<i64>());
+        let i0 = ixs.iter().position(|&i| i as usize == a.iter().position(|&x| x == 0).unwrap()).unwrap();
+        println!("{}", [1000, 2000, 3000].map(|di| a[ixs[(i0 + di) % n] as usize]).iter().sum::<i64>());
     }
 }
