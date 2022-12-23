e0dad4ac99e1026bf94c5b46e680dd7cac1dcc67
dan <guaneec@gmail.com>
2022-12-23 22:47:52 +0800

    Rust 2022.23 freebie hashset (.43s->.14s)

--- rust/2022/d23/src/main.rs
+++ rust/2022/d23/src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashSet;
+use fxhash::FxHashSet;
 
 const DIRS: [(i16, i16); 7] = [(-1,0), (1,0), (0,-1), (0,1), (-1,0), (1,0), (0,-1)];
 const D8: [(i16, i16); 8] = [(0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)];
 type Elf = (i16, i16);
-fn dest(elves: &HashSet<Elf>, elf: Elf, t: usize) -> Elf {
+fn dest(elves: &FxHashSet<Elf>, elf: Elf, t: usize) -> Elf {
     if D8.iter().all(|&(di, dj)| !elves.contains(&(di+elf.0, dj+elf.1))) { return elf; }
     for &(di, dj) in DIRS[t%4..t%4+4].iter() {
         if D8.iter().filter(|&(ddi, ddj)| di*ddi+dj*ddj == 1).all(|&(ddi, ddj)| !elves.contains(&(ddi+elf.0, ddj+elf.1))) {
@@ -15,7 +15,7 @@ fn dest(elves: &HashSet<Elf>, elf: Elf, t: usize) -> Elf {
 
 fn main() {
     let s = std::fs::read_to_string("../../../data/2022/23.input.txt").unwrap();
-    let elves: HashSet<_> = s.lines().enumerate().flat_map(|(i, l)| 
+    let elves: FxHashSet<_> = s.lines().enumerate().flat_map(|(i, l)| 
         l.chars().enumerate().filter(|&(_j, c)| c == '#').map(move |(j, _c)| (i as i16, j as i16))).collect();
     {
         let mut elves = elves.clone();
@@ -32,8 +32,7 @@ fn main() {
     {
         let mut elves = elves.clone();
         for i in 0.. {
-            let prev = step(&mut elves, i);
-            if prev == elves {
+            if !step(&mut elves, i) {
                 println!("{}", i+1);
                 break;
             }
@@ -41,16 +40,19 @@ fn main() {
     }
 }
 
-fn step(elves: &mut HashSet<(i16, i16)>, i: usize) -> HashSet<(i16, i16)> {
+fn step(elves: &mut FxHashSet<(i16, i16)>, i: usize) -> bool {
+    let mut moved = 0;
     let prev = elves.clone();
     elves.clear();
     for &e in &prev {
         let ee = dest(&prev, e, i);
+        moved += (ee != e) as u16;
         if !elves.insert(ee) {
             elves.remove(&ee);
             elves.insert(e);
             elves.insert((ee.0*2-e.0, ee.1*2-e.1));
+            moved -= 2;
         }
     }
-    prev
+    moved > 0
 }
