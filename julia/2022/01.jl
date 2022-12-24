f8adbb3800e3d0036b2d707dddc198ce6a54df2a
dan <guaneec@gmail.com>
2022-12-25 02:02:38 +0800

    filename

--- julia/2022/01.jl
+++ julia/2022/01.jl
@@ -1,3 +1,3 @@
-a = read("", String) |> strip |> s->split(s, "\n\n") |> g->map(sum ∘(a->map(s->parse(Int,s), a)) ∘ split, g) |> a->sort!(a,rev=true)
+a = read("../../data/2022/01.input.txt", String) |> strip |> s->split(s, "\n\n") |> g->map(sum ∘(a->map(s->parse(Int,s), a)) ∘ split, g) |> a->sort!(a,rev=true)
 a[1] |> println
 a[1:3] |> sum |> println
