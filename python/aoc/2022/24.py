5469c57c93a63b5e5e1952992a36f57a9e19dda5
dan <guaneec@gmail.com>
2022-12-24 15:16:33 +0800

    Python 2022.24

---
+++ python/aoc/2022/24.py
@@ -0,0 +1,68 @@
+from .util import getinput
+from ..aoc import astar
+s = getinput(24)
+m = len(s.splitlines())
+n = len(s.splitlines()[0])
+i0, i1 = 0, m-1
+lefts, rights, downs, ups = [], [], [], []
+for i, l in enumerate(s.splitlines()):
+    for j, c in enumerate(l):
+        if c == '.':
+            if i == i0:
+                j0 = j
+            if i == i1:
+                j1 = j
+        if c == '>': rights.append((i, j))
+        if c == '<': lefts.append((i, j))
+        if c == 'v': downs.append((i, j))
+        if c == '^': ups.append((i, j))
+z0 = (i0, j0)
+z1 = (i1, j1)
+cache = [set([*lefts, *rights, *downs, *ups])]
+def f(t):
+    global lefts, rights, downs, ups
+    if t < len(cache):
+        return cache[t]
+    assert t == len(cache)
+    ups = [(1+(ii-1+m-2-1)%(m-2), jj) for ii, jj in ups ]
+    downs = [(1+(ii-1+m-2+1)%(m-2), jj) for ii, jj in downs ]
+    lefts = [(ii, 1+(jj-1+n-2-1)%(n-2)) for ii, jj in lefts ]
+    rights = [(ii, 1+(jj-1+n-2+1)%(n-2)) for ii, jj in rights ]
+    cache.append(set([*lefts, *rights, *downs, *ups]))
+    return cache[-1]
+
+def nb(zt):
+    z, t = zt
+    i, j = z
+    assert t < 2000
+    yield from [(1, ((ii, jj), t+1)) for ii, jj in (
+        (i, j), (i+1,j), (i-1, j), (i, j+1), (i, j-1)
+    ) if (ii, jj ) in (z0, z1) or (0 < ii < m-1 and 0 < jj < n-1 and (ii, jj ) not in f(t+1))]
+
+def h(x):
+    i, j = x[0]
+    return abs(i1-i) + abs(j1-j)
+
+for d, nn in astar([(z0, 0)], nb, h):
+    if nn[0] == z1:
+        print(d)
+        break
+
+def nb(zt):
+    z, t, cps = zt
+    i, j = z
+    assert t < 2000
+    yield from [(1, ((ii, jj), t+1, cps + (z == (z1, z0)[cps % 2]))) for ii, jj in (
+        (i, j), (i+1,j), (i-1, j), (i, j+1), (i, j-1)
+    ) if (ii, jj ) in (z0, z1) or (0 < ii < m-1 and 0 < jj < n-1 and (ii, jj ) not in f(t+1))]
+
+def h(x):
+    z, t, cps = x
+    i, j = z
+    ii, jj = (z1, z0)[cps % 2]
+    return abs(i-ii) + abs(j-jj) + (2-cps) * (abs(i0-i1) + abs(j0-j1))
+
+for d, nn in astar([(z0, 0, 0)], nb, h):
+    if nn[-1] == 2 and nn[0] == z1:
+        print(d)
+        break
