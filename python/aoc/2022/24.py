0e2053545af18ba782fcfd15605711d4720af83a
dan <guaneec@gmail.com>
2022-12-24 16:02:09 +0800

    Python 2022.24 refactor (pypy 2.3s->0.4s)

--- python/aoc/2022/24.py
+++ python/aoc/2022/24.py
@@ -1,5 +1,5 @@
 from .util import getinput
-from ..aoc import astar, bfs
+from itertools import count
 s = getinput(24)
 m = len(s.splitlines())
 n = len(s.splitlines()[0])
@@ -18,41 +18,27 @@ for i, l in enumerate(s.splitlines()):
         if c == '^': ups.append((i, j))
 z0 = (i0, j0)
 z1 = (i1, j1)
-cache = [set([*lefts, *rights, *downs, *ups])]
-def f(t):
-    global lefts, rights, downs, ups
-    if t < len(cache):
-        return cache[t]
-    assert t == len(cache)
-    ups = [(1+(ii-1+m-2-1)%(m-2), jj) for ii, jj in ups ]
-    downs = [(1+(ii-1+m-2+1)%(m-2), jj) for ii, jj in downs ]
-    lefts = [(ii, 1+(jj-1+n-2-1)%(n-2)) for ii, jj in lefts ]
-    rights = [(ii, 1+(jj-1+n-2+1)%(n-2)) for ii, jj in rights ]
-    cache.append(set([*lefts, *rights, *downs, *ups]))
-    return cache[-1]
 
-def nb(zt):
-    z, t = zt
-    i, j = z
-    assert t < 2000
-    yield from [(((ii, jj), t+1)) for ii, jj in (
-        (i, j), (i+1,j), (i-1, j), (i, j+1), (i, j-1)
-    ) if (ii, jj ) in (z0, z1) or (0 < ii < m-1 and 0 < jj < n-1 and (ii, jj ) not in f(t+1))]
-
-for d, nn in bfs([(z0, 0)], nb):
-    if nn[0] == z1:
-        print(d)
-        break
-
-def nb(zt):
-    z, t, cps = zt
-    i, j = z
-    assert t < 2000
-    yield from [(((ii, jj), t+1, cps + (z == (z1, z0)[cps % 2]))) for ii, jj in (
-        (i, j), (i+1,j), (i-1, j), (i, j+1), (i, j-1)
-    ) if (ii, jj ) in (z0, z1) or (0 < ii < m-1 and 0 < jj < n-1 and (ii, jj ) not in f(t+1))]
-
-for d, nn in bfs([(z0, 0, 0)], nb):
-    if nn[-1] == 2 and nn[0] == z1:
-        print(d)
-        break
+def f(t0, start, end):
+    q = {start}
+    for t in count(t0):
+        assert q
+        blocked = {
+            *[(1+(ii-1-t-1)%(m-2), jj) for ii, jj in ups ],
+            *[(1+(ii-1+t+1)%(m-2), jj) for ii, jj in downs ],
+            *[(ii, 1+(jj-1-t-1)%(n-2)) for ii, jj in lefts ],
+            *[(ii, 1+(jj-1+t+1)%(n-2)) for ii, jj in rights ],
+        }
+        qq = []
+        for i, j in q:
+            if (i, j) == end:
+                return t
+            for ii, jj in ((i, j), (i+1,j), (i-1, j), (i, j+1), (i, j-1)):
+                if (ii, jj) in (start, end) or (0 < ii < m-1 and 0 < jj < n-1 and (ii, jj) not in blocked):
+                    qq.append((ii, jj))
+        q = set(qq)
+t1 = f(0, z0, z1)
+print(t1)
+t2 = f(t1, z1, z0)
+t3 = f(t2, z0, z1)
+print(t3)
