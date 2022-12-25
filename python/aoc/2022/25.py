a9b2cd28085a9c12ec8084f9be8e0a365ba710f2
dan <guaneec@gmail.com>
2022-12-25 13:13:24 +0800

    Python 2022.25

---
+++ python/aoc/2022/25.py
@@ -0,0 +1,27 @@
+from .util import getinput
+
+s = getinput(25)
+
+def f(w):
+    b = 1
+    o = 0
+    for c in w[::-1]:
+        o += (list('=-012').index(c)-2) * b
+        b *= 5
+    return o
+
+p1 = sum(map(f, s.strip().splitlines()))
+print(p1)
+
+def g(w):
+    while True:
+        if -2 <= w <= 2:
+            return '=-012'[w+2]
+        else:
+            r = w % 5
+            if r > 2:
+                r -= 5
+            return g((w-r)//5) + g(r)
+
+print(g(p1))
+            
