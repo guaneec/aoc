579a6f6b7e0d6bbcaf8d247cd51b60a83c4efb80
dan <guaneec@gmail.com>
2022-12-27 17:50:54 +0800

    Rust 2022.23 arrays (135ms->75ms)

--- rust/2022/d23/src/main.rs
+++ rust/2022/d23/src/main.rs
@@ -1,32 +1,43 @@
-use fxhash::FxHashSet;
-
 const DIRS: [(i16, i16); 7] = [(-1,0), (1,0), (0,-1), (0,1), (-1,0), (1,0), (0,-1)];
 const D8: [(i16, i16); 8] = [(0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)];
 type Elf = (i16, i16);
-fn dest(elves: &FxHashSet<Elf>, elf: Elf, t: usize) -> Elf {
-    if D8.iter().all(|&(di, dj)| !elves.contains(&(di+elf.0, dj+elf.1))) { return elf; }
-    for &(di, dj) in DIRS[t%4..t%4+4].iter() {
-        if D8.iter().filter(|&(ddi, ddj)| di*ddi+dj*ddj == 1).all(|&(ddi, ddj)| !elves.contains(&(ddi+elf.0, ddj+elf.1))) {
-            return (di+elf.0, dj+elf.1);
-        }
-    }
-    elf
+const R: usize = 128;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum State {
+    Empty,
+    Neutral,
+    Up,
+    Down,
+    Left,
+    Right,
 }
+const STATES: [State; 7] = [State::Up, State::Down, State::Left, State::Right, State::Up, State::Down, State::Left];
+
+#[derive(Clone)]
+struct Elves([[State; 2*R]; 2*R], Vec<Elf>);
 
 fn main() {
     let s = std::fs::read_to_string("../../../data/2022/23.input.txt").unwrap();
-    let elves: FxHashSet<_> = s.lines().enumerate().flat_map(|(i, l)| 
-        l.chars().enumerate().filter(|&(_j, c)| c == '#').map(move |(j, _c)| (i as i16, j as i16))).collect();
+    let mut elves = Elves([[State::Empty; 2*R]; 2*R], vec![]);
+    for (i, l) in s.lines().enumerate() {
+        for (j, c) in l.chars().enumerate() {
+            if c == '#' {
+                elves.0[i + R][j + R] = State::Neutral;
+                elves.1.push(((i+R) as i16, (j+R) as i16))
+            }
+        }
+    }
     {
         let mut elves = elves.clone();
         for i in 0..10 {
             step(&mut elves, i);
         }
-        let imn = elves.iter().map(|e| e.0).min().unwrap();
-        let imx = elves.iter().map(|e| e.0).max().unwrap();
-        let jmn = elves.iter().map(|e| e.1).min().unwrap();
-        let jmx = elves.iter().map(|e| e.1).max().unwrap();
-        println!("{}", (imx-imn+1)*(jmx-jmn+1)-elves.len() as i16);
+        let imn = elves.1.iter().map(|e| e.0).min().unwrap();
+        let imx = elves.1.iter().map(|e| e.0).max().unwrap();
+        let jmn = elves.1.iter().map(|e| e.1).min().unwrap();
+        let jmx = elves.1.iter().map(|e| e.1).max().unwrap();
+        println!("{}", (imx-imn+1)*(jmx-jmn+1)-elves.1.len() as i16);
     }
     
     {
@@ -40,19 +51,55 @@ fn main() {
     }
 }
 
-fn step(elves: &mut FxHashSet<(i16, i16)>, i: usize) -> bool {
+fn step(elves: &mut Elves, i: usize) -> bool {
+    // move up 
     let mut moved = 0;
-    let prev = elves.clone();
-    elves.clear();
-    for &e in &prev {
-        let ee = dest(&prev, e, i);
-        moved += (ee != e) as u16;
-        if !elves.insert(ee) {
-            elves.remove(&ee);
-            elves.insert(e);
-            elves.insert((ee.0*2-e.0, ee.1*2-e.1));
-            moved -= 2;
+    for &e in &elves.1 {
+        mark(&mut elves.0, e, i);
+    }
+    for (i, j) in elves.1.iter_mut() {
+        let x = elves.0[*i as usize][*j as usize];
+        let (ii, jj) = match x {
+            State::Up => (*i-1, *j),
+            State::Down => (*i+1, *j),
+            State::Left => (*i, *j-1),
+            State::Right => (*i, *j+1),
+            _ => { continue; }
+        };
+        if elves.0[ii as usize][jj as usize] == State::Empty {
+            elves.0[ii as usize][jj as usize] = State::Neutral;
+            moved += 1;
+        } else {
+            assert!(elves.0[ii as usize][jj as usize] == State::Neutral);
+            elves.0[ii as usize][jj as usize] = State::Empty;
+            elves.0[*i as usize][*j as usize] = State::Neutral;
+            elves.0[(ii*2-*i) as usize][(jj*2-*j) as usize] = State::Neutral;
+            moved -= 1;
         }
     }
+    for (i, j) in elves.1.iter_mut() {
+        let x = elves.0[*i as usize][*j as usize];
+        if x == State::Neutral { 
+            continue; 
+        }
+        elves.0[*i as usize][*j as usize] = State::Empty;
+        (*i, *j) = match x {
+            State::Up => (*i-1, *j),
+            State::Down => (*i+1, *j),
+            State::Left => (*i, *j-1),
+            State::Right => (*i, *j+1),
+            _ => { panic!(); }
+        };
+    }
     moved > 0
+}
+
+fn mark(elves: &mut [[State; 2*R]; 2*R], elf: Elf, t: usize) {
+    if D8.iter().all(|&(di, dj)| elves[(di+elf.0) as usize][(dj+elf.1) as usize] == State::Empty) { return; }
+    for (&(di, dj), &state) in DIRS[t%4..t%4+4].iter().zip(STATES[t%4..t%4+4].iter()) {
+        if D8.iter().filter(|&(ddi, ddj)| di*ddi+dj*ddj == 1).all(|&(ddi, ddj)| elves[(ddi+elf.0) as usize][(ddj+elf.1) as usize] == State::Empty) {
+            elves[elf.0 as usize][elf.1 as usize] = state;
+            return;
+        }
+    }
 }
