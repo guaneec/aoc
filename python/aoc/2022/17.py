561e50c5fc239e66361287441a78f9947cff96f5
dan <guaneec@gmail.com>
2022-12-29 02:37:44 +0800

    Python 2022.17 part2

--- python/aoc/2022/17.py
+++ python/aoc/2022/17.py
@@ -3,7 +3,7 @@ from itertools import cycle
 
 s = getinput(17).strip()
 
-rocks = '''####
+rocks = """####
 
 .#.
 ###
@@ -20,34 +20,93 @@ rocks = '''####
 
 ##
 ##
-'''
+"""
 
-rr = []
-for g in rocks.split('\n\n'):
-    r = []
-    mini = 1000
-    maxj = -1
-    for j, l in enumerate(g.splitlines()):
-        for i, c in enumerate(l):
-            if c == '#':
-                r.append((i, j))
-                mini = min(mini, i)
-                maxj = max(maxj, j)
-    rr.append([(i-mini, maxj-j) for i, j in r])
+rocks = [
+    [
+        (i, j)
+        for j, l in enumerate(reversed(g.splitlines()))
+        for i, c in enumerate(l)
+        if c == "#"
+    ]
+    for g in rocks.split("\n\n")
+]
 
+RIGHT = 5
+LEFT = -3
+FLOOR = 0
 tip = 0
 placed = set()
-it = cycle((j, 1 if c == '>' else -1) for j, c in enumerate(s))
-for rock, _ in zip(cycle(rr), range(2022)):
+
+
+class Recorder:
+    def __init__(self) -> None:
+        self.enabled = False
+        self.logs = []
+
+    def start(self, tip, placed, ir, ig):
+        self.enabled = True
+        self.tip = tip
+        self.placed = {*placed}
+        self.ir = ir
+        self.ig = ig
+
+    def add_record(self, bumped, rock):
+        if self.enabled and (not bumped or will_bump(rock, self.placed)):
+            self.logs.append((bumped, rock))
+
+    def can_replay(self, tip, placed, ir, ig):
+        return (
+            self.logs
+            and (self.ir - ir) % len(rocks) == 0
+            and (self.ig - ig) % len(s) == 0
+            and all(
+                bumped
+                == will_bump(((x, y + tip - self.tip) for x, y in blocks), placed)
+                for bumped, blocks in self.logs
+            )
+        )
+
+
+def will_bump(rock, placed):
+    return any(
+        x == RIGHT or x == LEFT or y == FLOOR or (x, y) in placed for x, y in rock
+    )
+
+
+offset = 0
+target = 1000000000000
+GUESS = 3000  # must be in a cycle after this many rocks
+recorder = Recorder()
+ig = 0
+for ir, rock in enumerate(cycle(rocks)):
+    if recorder.can_replay(tip, placed, ir, ig):
+        P = ir - recorder.ir
+        skipped = (target - ir) // P
+        target -= skipped * P
+        offset += skipped * (tip - recorder.tip)
+    if ir >= GUESS and ir % len(rr) == target % len(rr):
+        if not recorder.enabled:
+            recorder.start(tip, placed, ir, ig)
+    if ir == 2022:
+        print(tip)
+    if ir == target:
+        print(tip + offset)
+        break
     r = [(x, y + tip + 4) for x, y in rock]
     while True:
-        j, dx = next(it)
-        if not any(x + dx == 5 or x + dx == -3 or (x+dx, y) in placed  for x, y in r):
-            r = [(x + dx, y) for x, y in r]
-        if not any(y - 1 == 0 or (x, y-1) in placed for x, y in r):
-            r = [(x, y-1) for x, y in r]
+        dx = -1 if s[ig % len(s)] == "<" else 1
+        ig += 1
+        rr = [(x + dx, y) for x, y in r]
+        if not (bumped := will_bump(rr, placed)):
+            r = rr
+        recorder.add_record(bumped, rr)
+        rr = [(x, y - 1) for x, y in r]
+        bumped = will_bump(rr, placed)
+        recorder.add_record(bumped, rr)
+        if not bumped:
+            r = rr
         else:
             break
     tip = max(tip, max(y for x, y in r))
     placed.update(r)
-print(tip)
